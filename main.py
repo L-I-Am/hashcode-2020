a44689575327cff040e7c2388c9d8d680f4efe36
Liam Oorts <liam.oorts@aloxy.io>
2020-02-20 19:26:58 +0100

    init commit

---
+++ main.py
@@ -0,0 +1,46 @@
+class Book:
+    def __init__(self, id, score):
+        self.id = id
+        self.score = score
+
+
+class Lib:
+    def __init__(self, id, books, signup_time, score):
+        self.id = id
+        self.books = books
+        self.signup_time = signup_time
+        self.score = score
+
+
+class Problem:
+    def __init__(self, days, libs):
+        self.days = days
+        self.libs = libs
+
+
+def solver(problem):
+    pass
+
+
+def read_input(filepath,
+               n_lines=None):
+    with open(filepath) as fp:
+        lines = fp.readlines()
+    lines = [x.strip() for x in lines]
+    if n_lines:
+        return lines[:n_lines]
+    else:
+        return lines
+
+
+def write_output(data, filepath):
+    with open(filepath, 'w') as fp:
+        for line in data:
+            for number in line:
+                fp.write(str(number))
+                fp.write(' ')
+            fp.write('\n')
+
+
+if __name__ == "__main__":
+    data = read_input('a_example.txt')
